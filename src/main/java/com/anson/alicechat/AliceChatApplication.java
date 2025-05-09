6a0045838bf4af2dd69499709af905ad3ba2e3a5
code-with-Anson <2795757650@qq.com>
2025-05-09 21:28:41 +0800

    爱丽丝的大冒险要开始咯~

---
+++ src/main/java/com/anson/alicechat/AliceChatApplication.java
@@ -0,0 +1,13 @@
+package com.anson.alicechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliceChatApplication.class, args);
+    }
+
+}
